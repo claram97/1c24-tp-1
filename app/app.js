c211fdbf3415c77f2605466cdeba0776ea20879f
mauro-rizzi-DSP <mauro.rizzi@despegar.com>
2024-04-13 18:58:58 -0300

    Implementacion de endpoint quote

--- app/app.js
+++ app/app.js
@@ -53,17 +53,35 @@ app.get('/spaceflight_news', async (req, res) => {
       }
 });
 
+app.get('/quote', async (req, res) => {
+    console.log(`Pedido de noticias sobre el espacio`);
+
+    try {
+        const result = await axios.get(`https://api.quotable.io/quotes/random`);
+
+        const quote = result.data.map(quote => ({
+                      quote: quote.content,
+                      author: quote.author
+                    }));
+
+        res.status(200).json(quote[0]);
+      } catch (error) {
+        console.error('Error obteniendo resultado desde quotable:', error);
+        res.status(500).send('Error when retrieving quote, contact your service administrator');
+      }
+});
+
 
 // Manejador para rutas no encontradas
 app.use((req, res) => {
-    res.status(404).json({ error: 'Ruta no encontrada' });
+    res.status(404).json({ error: 'Page not found' });
     console.log("Pedido sobre ruta no existente")
 });
 
 // Manejador de errores
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({ error: 'Error interno del servidor' });
+    res.status(500).json({ error: 'Internal server error' });
 });
 
 // Iniciamos el servidor
