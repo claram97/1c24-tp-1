0e665633ce67a60c90ca18be82e2089a1ade58f5
mauro-rizzi-DSP <mauro.rizzi@despegar.com>
2024-04-13 18:45:49 -0300

    Implementacion de endpoint spaceflight_news

--- app/app.js
+++ app/app.js
@@ -32,9 +32,27 @@ app.get('/dictionary', async (req, res) => {
         res.status(200).json(definitions);
       } catch (error) {
         console.error('Error obteniendo resultado desde dictionaryapi:', error);
-        res.status(500).send('Error al consultar al diccionario');
+        res.status(500).send('Error when consulting the dictionary, contact your service administrator');
       }
 });
+
+const HEADLINE_COUNT = 5;
+app.get('/spaceflight_news', async (req, res) => {
+    console.log(`Pedido de noticias sobre el espacio`);
+
+    try {
+        const result = await axios.get(`https://api.spaceflightnewsapi.net/v4/articles/?limit=${HEADLINE_COUNT}`);
+        const titles = [];
+
+        result.data.results.forEach(result => {titles.push(result.title)} )
+
+        res.status(200).json(titles);
+      } catch (error) {
+        console.error('Error obteniendo resultado desde spaceflightnewsapi:', error);
+        res.status(500).send('Error when consulting the news, contact your service administrator');
+      }
+});
+
 
 // Manejador para rutas no encontradas
 app.use((req, res) => {
