3c0f520ff2d5df02d0356904e863bc1b83cf4a35
mauro-rizzi-DSP <mauro.rizzi@despegar.com>
2024-04-13 18:30:12 -0300

    Implementacion de endpoint dictionary

--- app/app.js
+++ app/app.js
@@ -8,6 +8,34 @@ app.get('/ping', (req, res) => {
     res.status(200).send("Pong!");
 });
 
+app.get('/dictionary', async (req, res) => {
+    const word = req.query.word;
+    console.log(`Pedido de dictionary sobre la palabra ${word}`);
+
+    try {
+        const result = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+
+        const definitions = result.data.map(entry => ({
+              word: entry.word,
+              phonetic: entry.phonetic,
+              meanings: entry.meanings.map(meaning => ({
+                partOfSpeech: meaning.partOfSpeech,
+                definitions: meaning.definitions.map(definition => ({
+                  definition: definition.definition,
+                  synonyms: definition.synonyms,
+                  antonyms: definition.antonyms,
+                  example: definition.example
+                }))
+              }))
+            }));
+
+        res.status(200).json(definitions);
+      } catch (error) {
+        console.error('Error obteniendo resultado desde dictionaryapi:', error);
+        res.status(500).send('Error al consultar al diccionario');
+      }
+});
+
 // Manejador para rutas no encontradas
 app.use((req, res) => {
     res.status(404).json({ error: 'Ruta no encontrada' });
