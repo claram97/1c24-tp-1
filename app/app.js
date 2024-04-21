add18f42c4d645581503532a9d38eaa41cad7caf
claram97 <cruano@fi.uba.ar>
2024-04-21 13:46:49 -0300

    se ven las métricas propias en graphite

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const axios = require('axios');
-// const StatsD = require('node-statsd');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,13 +21,8 @@ process.on('SIGTERM', async () => {
   console.log('Disconnected from Redis');
 });
 
-// const dogstatsd = new StatsD({
-//   host: 'graphite',
-//   port: 8125,
-// });
-
 var StatsD = require('hot-shots'),
-dogstatsd = new StatsD({
+myStats = new StatsD({
   host: 'graphite',  
   port: 8125
 });
@@ -42,8 +36,8 @@ app.use((req, res, next) => {
 app.get('/ping', (req, res) => {
     res.status(200).send("Pong!");
     const responseTime = Date.now() - req.startTime;
-    dogstatsd.gauge(`throughput.ping_response_time`, responseTime);
-    dogstatsd.gauge(`latency.ping_latency`, responseTime);
+    myStats.gauge(`throughput.ping_response_time`, responseTime);
+    myStats.gauge(`latency.ping_latency`, responseTime);
 });
 
 const CACHE_EXPIRATION_SECONDS = 10; // Tiempo de expiración en segundos
@@ -57,15 +51,15 @@ app.get('/dictionary', async (req, res) => {
     if (cachedDefinition) {
       console.log(`La palabra ${word} se encontró en la caché de Redis.`);
       const responseTime = Date.now() - req.startTime;
-      dogstatsd.timing(`throughput.dictionary_response_time`, responseTime);
-      dogstatsd.timing(`latency.dictionary_latency`, responseTime);
+      myStats.gauge(`throughput.dictionary_response_time`, responseTime);
+      myStats.gauge(`latency.dictionary_latency`, responseTime);
       return res.status(200).json(JSON.parse(cachedDefinition));
     }
     try {
       // Si la palabra no está en caché, hacer la solicitud a la API
       const result = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
       const latency = Date.now() - req.startTime;
-      dogstatsd.timing(`latency.dictionary_latency`, latency);
+      myStats.gauge(`latency.dictionary_latency`, latency);
 
       const definitions = result.data.map(entry => ({
           word: entry.word,
@@ -86,7 +80,7 @@ app.get('/dictionary', async (req, res) => {
 
       res.status(200).json(definitions);
       const responseTime = Date.now() - req.startTime;
-      dogstatsd.timing(`throughput.dictionary_response_time`, responseTime);
+      myStats.gauge(`throughput.dictionary_response_time`, responseTime);
     } catch (error) {
         console.error('Error obteniendo resultado desde dictionaryapi:', error);
         res.status(500).send('Error when consulting the dictionary, contact your service administrator');
@@ -105,16 +99,16 @@ app.get('/spaceflight_news', async (req, res) => {
     if (spaceNews) {
       titles = JSON.parse(spaceNews);
       const latency = Date.now() - req.startTime;
-      dogstatsd.timing(`latency.space_news_latency`, latency);
+      myStats.gauge(`latency.space_news_latency`, latency);
       const responseTime = Date.now() - req.startTime;
-      dogstatsd.timing(`throughput.space_news_response_time`, responseTime);
+      myStats.gauge(`throughput.space_news_response_time`, responseTime);
       return res.status(200).json(titles);
     }
     else {
       try {
         const result = await axios.get(`https://api.spaceflightnewsapi.net/v4/articles/?limit=${HEADLINE_COUNT}`);
         const latency = Date.now() - req.startTime;
-        dogstatsd.timing(`latency.space_news_latency`, latency);
+        myStats.gauge(`latency.space_news_latency`, latency);
         result.data.results.forEach(result => {titles.push(result.title)} )
       } catch (error) {
         console.error('Error obteniendo resultado desde spaceflightnewsapi:', error);
@@ -125,7 +119,7 @@ app.get('/spaceflight_news', async (req, res) => {
     redisClient.set('space-news', JSON.stringify(titles), {EX: SPACE_NEWS_EXPIRATION});
     res.status(200).json(titles);
     const responseTime = Date.now() - req.startTime;
-    dogstatsd.timing(`throughput.space_news_response_time`, responseTime);
+    myStats.gauge(`throughput.space_news_response_time`, responseTime);
 });
 
 app.get('/quote', async (req, res) => {
@@ -134,13 +128,13 @@ app.get('/quote', async (req, res) => {
     try {
         const result = await axios.get(`https://api.quotable.io/quotes/random`);
         const latency = Date.now() - req.startTime;
-        dogstatsd.timing(`latency.quote_latency`, latency);
+        myStats.gauge(`latency.quote_latency`, latency);
         const quote = result.data.map(quote => ({
                       quote: quote.content,
                       author: quote.author
                     }));
         const responseTime = Date.now() - req.startTime;
-        dogstatsd.timing(`latency.quote_latency`, responseTime);
+        myStats.gauge(`latency.quote_response_time`, responseTime);
         return res.status(200).json(quote[0]);
       } catch (error) {
         console.error('Error obteniendo resultado desde quotable:', error);
